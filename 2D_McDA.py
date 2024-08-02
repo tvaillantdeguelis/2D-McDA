733074b5392b8257df337959fc2968c374623142
Thibault Vaillant de Guélis <thibault.vaillantdeguelis@outlook.com>
2024-08-02 15:22:11 +0200

    Update submodule to v1.0.6

--- 2D_McDA.py
+++ 2D_McDA.py
@@ -15,9 +15,10 @@ from datetime import datetime
 import numpy as np
 
 from my_modules.standard_outputs import print_time, print_elapsed_time
-from my_modules.readers.calipso_reader import CALIOPRegularGridReader, split_granule_date
+from my_modules.readers.calipso_reader import CALIOPRegularGridReader
 from my_modules.calipso_constants import *
 from my_modules.writers.hdf_writer import SDSData, write_hdf
+from my_modules.paths import split_granule_date
 
 from config import NB_PROF_SLICE, NB_PROF_OVERLAP, NB_PROF_EDGE
 from surface_detection import detect_surface
